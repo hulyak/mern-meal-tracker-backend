8ffe0b7fbce20ba24c9ed3f7048ac9979e8f1142
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 15:55:10 -0800

    create the add ingredients page

--- src/routes/addIngredientRoute.js
+++ src/routes/addIngredientRoute.js
@@ -1,5 +1,13 @@
+import { getIngredients, insertIngredient } from '../db';
+
 export const addIngredientRoute = {
   method: 'post',
   path: '/ingredients',
-  handler: async (req, res) => {},
+  handler: async (req, res) => {
+    // send a post request from frontend, with the information about what we want to add
+    const ingredient = req.body;
+    await insertIngredient(ingredient);
+    const updatedIngredients = await getIngredients();
+    res.status(200).json(updatedIngredients);
+  },
 };
