7211cbec85c68612d5f4ec11f2ee7b6c18309613
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 12:57:41 -0800

    bring the ingredients data and connect to frontend

--- src/routes/getIngredientsRoute.js
+++ src/routes/getIngredientsRoute.js
@@ -1,5 +1,10 @@
+import { getIngredients } from '../db';
+
 export const getIngredientsRoute = {
   method: 'get',
   path: '/ingredients',
-  handler: async (req, res) => {},
+  handler: async (req, res) => {
+    const ingredients = await getIngredients();
+    res.status(200).json(ingredients);
+  },
 };
