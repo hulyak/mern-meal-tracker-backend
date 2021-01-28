3dd4a1cae58327f2a094094098876e008d4b9b53
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 13:14:03 -0800

    create the delete Meals route and helper functions

--- src/routes/deleteMealRoute.js
+++ src/routes/deleteMealRoute.js
@@ -1,5 +1,12 @@
+import { deleteMeal, getPopulatedMeals } from "../db";
+
 export const deleteMealRoute = {
   method: 'delete',
   path: '/meals/:id',
-  handler: async (req, res) => {},
+  handler: async (req, res) => {
+    const { id } = req.params;
+    await deleteMeal(id);
+    const updatedMeals = await getPopulatedMeals();
+    return updatedMeals;
+  },
 };
