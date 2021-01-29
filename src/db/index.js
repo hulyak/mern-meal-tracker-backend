95805816990a5d36151e7d989a141ad3843d1602
hulya <hulyakarakayaa06@gmail.com>
2021-01-28 18:54:35 -0800

    create insert meal and add meal routes

--- src/db/index.js
+++ src/db/index.js
@@ -5,3 +5,4 @@ export { deleteMeal } from './deleteMeal'
 export { deleteIngredient } from './deleteIngredient';
 export { insertIngredient } from './insertIngredient'
 export { searchRecipes } from './searchRecipes'
+export { insertMeal } from './insertMeal';
